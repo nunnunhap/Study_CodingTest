cd2b16a9db5f090543ae55fd1c6f27491fceba26
nunnunhap <yew240227@gmail.com>
2025-01-03 23:31:00 +0900

    10171 고양이

---
+++ src/main/java/chap01/problem12/Main.java
@@ -0,0 +1,10 @@
+package chap01.problem12;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+    }
+}
