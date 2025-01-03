f7d318703c88b48043efdbfbd6ae37d3479c7a83
nunnunhap <yew240227@gmail.com>
2025-01-03 19:12:27 +0900

    2557 Hello World

---
+++ src/test/java/chap01/problem01/MainTest.java
@@ -0,0 +1,12 @@
+package chap01.problem01;
+
+import org.junit.jupiter.api.Test;
+
+class MainTest {
+
+    @Test
+    void testMainOutput() {
+        // Main 클래스의 main 메서드 실행
+        Main.main(new String[]{});
+    }
+}
