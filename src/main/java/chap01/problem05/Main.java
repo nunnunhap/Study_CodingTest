afd529d555b2a5f1b5d93e5198a6b845b7d88e4c
nunnunhap <yew240227@gmail.com>
2025-01-03 22:41:06 +0900

    1008 A/B

---
+++ src/main/java/chap01/problem05/Main.java
@@ -0,0 +1,16 @@
+package chap01.problem05;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        double A = scanner.nextInt();
+        double B = scanner.nextInt();
+
+        System.out.println(A/B);
+
+        scanner.close();
+    }
+}
