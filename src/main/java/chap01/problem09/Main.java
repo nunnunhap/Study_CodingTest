f8de774c1c62acba01dadbcdd0a3d9b89a63282b
nunnunhap <yew240227@gmail.com>
2025-01-03 23:04:11 +0900

    10430 나머지

---
+++ src/main/java/chap01/problem09/Main.java
@@ -0,0 +1,18 @@
+package chap01.problem09;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int A = scanner.nextInt();
+        int B = scanner.nextInt();
+        int C = scanner.nextInt();
+        scanner.close();
+
+        System.out.println((A+B)%C);
+        System.out.println(((A%C) + (B%C))%C);
+        System.out.println((A*B)%C);
+        System.out.println(((A%C) * (B%C))%C);
+    }
+}
