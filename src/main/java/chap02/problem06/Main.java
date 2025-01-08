0a83b8b609ac4b4e19ac228afb6c614649330fa9
nunnunhap <yew240227@gmail.com>
2025-01-08 22:02:01 +0900

    2525 오븐 시계

---
+++ src/main/java/chap02/problem06/Main.java
@@ -0,0 +1,21 @@
+package chap02.problem06;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int A = scanner.nextInt();
+        int B = scanner.nextInt();
+        int C = scanner.nextInt();
+
+        int min = 60 * A + B;
+        min += C;
+
+        int hour = (min / 60) % 24;
+        int minute = min % 60;
+
+        System.out.println(hour + " " + minute);
+    }
+}
